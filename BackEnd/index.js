0f6a2b9b3d1b592dd0edfca7d7a4561f9e77dc14
camiloalvarez98 <camiloalvarezduarte@gmail.com>
2022-07-07 01:04:23 -0400

    c

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -19,9 +19,9 @@ app.use('/api/Jugador',require('./src/Routes/jugador.routes'));
 app.use('/api/Administrador',require('./src/Routes/administrador.routes'));
 app.use('/api/Club',require('./src/Routes/club.routes'));
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 //inicializador del server
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+    console.log(`Server listening on port 3000`);
 });
